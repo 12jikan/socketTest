beb361abbee795f6f0168022c844ec23a2a011ef
12jikan <jareem.e.hoff@gmail.com>
2019-10-08 03:11:53 -0400

    first amount of work in socket

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-"" 
+const app = require('express')();
+const port = 3000;
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    // res.sendFile( __dirname + 'index.html');
+    console.log(res.sendFile(__dirname + '/index.html'));
+});
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(3000, () => {
+    console.log(`listening on port: http://localhost:${port}`);
+})
